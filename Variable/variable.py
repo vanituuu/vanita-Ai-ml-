0b071e24fb7c1707df958cfe93a1332430cfbdf7
vanituuu <vanitakumari00786@gmail.com>
2025-11-24 00:34:46 +0530

    Update variable.py

--- Variable/variable.py
+++ Variable/variable.py
@@ -1,1 +1,35 @@
+# Variables in Python
+
+first_name = 'Vanita'
+last_name = 'Kumari'
+country = 'India'
+city = 'Chandigarh'
+age = 90
+person_info = {
+    'firstname':'Vanita', 
+    'lastname':'Kumari', 
+    'country':'India',
+    'city':'Chandiagrh'
+    }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age= 'Vanita', 'Kumari', 'India',90
+
+print(first_name, last_name, country, age)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
 
