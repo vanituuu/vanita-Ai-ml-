ad64b705745d69b3650f184e57dfdc7b7205dd04
vanituuu <vanitakumari00786@gmail.com>
2025-11-24 15:51:00 +0530

    Update Operators.py

--- Operators/Operators.py
+++ Operators/Operators.py
@@ -1,1 +1,28 @@
+# Arithmetic Operator
+# Variables
+a = 15
+b = 4
+
+# Addition
+print("Addition:", a + b)  
+
+# Subtraction
+print("Subtraction:", a - b) 
+
+# Multiplication
+print("Multiplication:", a * b)  
+
+# Division
+print("Division:", a / b) 
+
+# Floor Division
+print("Floor Division:", a // b)  
+
+# Modulus
+print("Modulus:", a % b) 
+
+# Exponentiation
+print("Exponentiation:", a ** b)
+
+
 
