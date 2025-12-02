f4df98d4eced1acec30778ca9a9bd9d60234df3b
vanituuu <vanitakumari00786@gmail.com>
2025-12-02 12:40:52 +0530

    Update LOOPS.py

--- LOOPS/LOOPS.py
+++ LOOPS/LOOPS.py
@@ -1,5 +1,5 @@
 # While loops
-
+# 1. While loops
 n=1
 while n<11:
   n+=1
@@ -39,4 +39,40 @@ if reverse ==temp_num:
 else:
   print("Number is not palindrome")
 
+# 5. Armstrong number.
+num_str = input("Enter the number:") # Get input as string
+num = int(num_str) # Convert to integer for calculations
+length = len(num_str) # Get length from the string
+print(length)
 
+sum = 0
+original_num = num # Store the original number to compare later
+while num > 0:
+  digit = num % 10
+  sum += digit ** length
+  num = num // 10
+print(sum)
+
+if sum == original_num: # Compare the sum with the original number
+    print("This is an Armstrong number.")
+else:
+    print("This is not an Armstrong number.")
+
+
+# LCM using while loop.
+num1 = int(input("Enter the  number:"))
+num2 = int(input("Enter the number:"))
+Greater = 0
+Multiplication = num1*num2
+
+if num1>num2:
+  Greater = num1
+else:
+  Greater = num2
+
+i = Greater
+while i<= Multiplication:
+    if i%num1==0 and i%num2==0:
+      LCM = i
+    i+=1
+print(i)
