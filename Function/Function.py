4bf57968447b99d14bf5f59cf6de26847ba5371f
Vanita <vanitakumari00786@gmail.com>
2025-12-11 23:05:33 +0530

    Update Function.py

--- Function/Function.py
+++ Function/Function.py
@@ -8,3 +8,11 @@ def EvenOdd(x):
 print(EvenOdd(16))
 print(EvenOdd(5))
 
+# sum of square of first n natural number:
+def square_no(num):
+    num = int(input("Enter the number:"))
+    if num<0:
+        raise ValueError("num not ne non-nehative")
+    return(num*(num+1)*(2*num+1))//6
+print(square_no("num"))
+
